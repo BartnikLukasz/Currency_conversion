a6b7cf192bcf379e554494bb6822a3fec4f47f0b
BartnikLukasz <bartnik.luk@gmail.com>
2021-06-24 12:03:49 +0200

    Added tests

--- src/test/java/przewalutowanie/conversion/CurrencyConversionApplicationTests.java
+++ src/test/java/przewalutowanie/conversion/CurrencyConversionApplicationTests.java
@@ -2,12 +2,29 @@ package przewalutowanie.conversion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CurrencyConversionApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void conversionTest(){
+		RestTemplate restTemplate = new RestTemplate();
+		double value = Double.parseDouble(restTemplate.getForObject("http://localhost:8080/convert?currencyFromCode=EUR&currencyToCode=EUR&currencyFromValue=10", String.class));
+		assertTrue(value<10);
+	}
+
+	@Test
+	void currencyTest(){
+		RestTemplate restTemplate = new RestTemplate();
+		assertEquals("200 OK", restTemplate.getForEntity("http://localhost:8080/convert?currencyFromCode=BBB&currencyToCode=EUR&currencyFromValue=10", String.class).getStatusCode().toString());
+		assertEquals("This currency cannot be converted or value is less then 0", restTemplate.getForObject("http://localhost:8080/convert?currencyFromCode=BBB&currencyToCode=EUR&currencyFromValue=10", String.class));
 	}
 
 }
